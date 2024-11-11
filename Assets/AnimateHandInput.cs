d9d86f1dbd164765968eff993256b2a39ab95bdd
HWL-itb-Santiago <santiago.vergara.7e8@itb.cat>
2024-11-11 23:09:22 +0100

    Add new hands animations for the template

--- Assets/AnimateHandInput.cs
+++ Assets/AnimateHandInput.cs
@@ -1,41 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
+namespace UnityEngine.XR.Interaction.Toolkit.Inputs
+{
     public class AnimateHandInput : MonoBehaviour
     {
         public Animator handAnimator;
+        public InputActionManager inputActionManager;
         public InputActionReference handActionGrab;
         public InputActionReference handActionTrigger;
         // Start is called before the first frame update
-        void OnEnable()
+        void Start()
         {
-
+            if (inputActionManager != null)
+            {
+                // Suscribirse a los eventos de las acciones
+                if (handActionGrab != null)
+                {
+                    handActionGrab.action.performed += OnGripHand;
+                    handActionGrab.action.canceled += OnGripHand;
+                }
+                if (handActionTrigger != null)
+                {
+                    handActionTrigger.action.performed += OnTriggerHand;
+                    handActionTrigger.action.canceled += OnTriggerHand;
+                }
+            }
+            else
+            {
+                Debug.LogError("InputActionManager no asignado en AnimateHandInput.");
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
         }
-
         private void FixedUpdate()
         {
         }
-
-        public void OnActivateValue(InputAction.CallbackContext context)
+        public void OnGripHand(InputAction.CallbackContext context)
         {
             float handGrabValue = context.ReadValue<float>();
+            Debug.Log("Grab Value" + handGrabValue);
             handAnimator.SetFloat("Grip", handGrabValue);
         }
 
-        public void OnActivate(InputAction.CallbackContext context)
+        public void OnTriggerHand(InputAction.CallbackContext context)
         {
             float handTriggerValue = context.ReadValue<float>();
+            Debug.Log("Trigger Value" + handTriggerValue);
             handAnimator.SetFloat("Trigger", handTriggerValue);
         }
+
     }
+}
 
